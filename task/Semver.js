1bb2fa969a450b3ec3ad3e83ab1c7fcedc4103b0
Denis <denis_golovin@mail.ua>
2017-02-19 19:48:35 +0200

    a tak?

--- task/Semver.js
+++ task/Semver.js
@@ -1,8 +1,9 @@
 function Semver(initial) {
 	this.version = initial || '0.0.0'
-	this.c = 0;
-	this.b = 0;
-	this.a = 0;
+	var newArr = initial.split('.');
+	this.c = newArr[2] || 0;
+	this.b = newArr[1] || 0;
+	this.a = newArr[0] || 0;
 }
 
 Semver.prototype.patch = function () {
@@ -14,5 +15,8 @@ Semver.prototype.minor = function () {
 }
 
 Semver.prototype.major = function () {
+	this.version = ++this.a + '.' + (this.b = 0) + '.' + (this.c = 0);
+}
+function () {
 	this.version = ++this.a + '.' + (this.b = 0) + '.' + (this.c = 0);
 }
